bdf3a1c23daa7147e0638aea3f17e1c54b9f05c7
sawit1237 <ayahciwas@gmail.com>
2016-11-19 13:39:02 +0700

    pppp

--- app/src/main/java/su/ict/business59/partnersforpurchasing/MainActivity.java
+++ app/src/main/java/su/ict/business59/partnersforpurchasing/MainActivity.java
@@ -2,12 +2,14 @@ package su.ict.business59.partnersforpurchasing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et= (EditText) findViewById(R.id.user);
     }
 }

8a2674631d2b4650c1d274c0d23054801c5d589f
panya.bas <th.panya.bas@gmail.com>
2017-02-05 22:03:23 +0700

    login fb

--- app/src/main/java/su/ict/business59/partnersforpurchasing/MainActivity.java
+++ app/src/main/java/su/ict/business59/partnersforpurchasing/MainActivity.java
@@ -1,6 +1,7 @@
 package su.ict.business59.partnersforpurchasing;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,10 +10,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.HashMap;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -25,7 +49,7 @@ import su.ict.business59.partnersforpurchasing.utills.ServiceGenerator;
 import su.ict.business59.partnersforpurchasing.utills.UpdateStatusUser;
 import su.ict.business59.partnersforpurchasing.utills.UserPreference;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Bind(R.id.user)
     EditText et;
@@ -37,6 +61,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button bt2;
     private UserPreference pref;
     private Gson gson;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private AccessTokenTracker accessTokenTracker;
+    private ProfileTracker profileTracker;
+    public static final String MULTIPART_FORM_DATA = "multipart/form-data";
+    //facebook
+    // https://www.sitepoint.com/integrating-the-facebook-api-with-android/
+
+    // Callback registration
+    private FacebookCallback<LoginResult> callback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            Profile profile = Profile.getCurrentProfile();
+            nextActivity(profile);
+        }
+
+        @Override
+        public void onCancel() {
+        }
+
+        @Override
+        public void onError(FacebookException e) {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,11 +97,75 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        initFacebookBtn();
         bt1.setOnClickListener(this);
         bt2.setOnClickListener(this);
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+    }
+
+    private void initFacebookBtn() {
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken newToken) {
+            }
+        };
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
+                nextActivity(newProfile);
+            }
+        };
+//        accessTokenTracker.startTracking();
+//        profileTracker.startTracking();
+
+        // callback on click button login
+        callback = new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                Profile profile = Profile.getCurrentProfile();
+                nextActivity(profile);
+                // Facebook Email address
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.v("LoginActivity Response ", response.toString());
+                        try {
+                            String name = object.getString("name");
+                            String email = object.getString("email");
+                            String gender = object.getString("gender");
+                            Log.v("Email = ", email);
+                            Toast.makeText(getApplicationContext(), "Name " + name, Toast.LENGTH_LONG).show();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+                Toast.makeText(getApplicationContext(), "Logging in...", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+                LoginManager.getInstance().logOut();
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+            }
+        };
+
+        loginButton.setReadPermissions("user_friends");
+        loginButton.registerCallback(callbackManager, callback);
     }
 
     @Override
@@ -100,7 +212,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else {
             Toast.makeText(getApplicationContext(), "Fill in the blank", Toast.LENGTH_SHORT).show();
         }
-
     }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //Facebook login
+        Profile profile = Profile.getCurrentProfile();
+        nextActivity(profile);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        //Facebook login
+        accessTokenTracker.stopTracking();
+        profileTracker.stopTracking();
+    }
+
+    private void nextActivity(Profile profile) {
+        if (profile != null) {
+            Log.v("FirstName", profile.getFirstName());
+            Log.v("LastName", profile.getLastName());
+            Log.v("ID", profile.getId());
+            Log.v("Img", profile.getProfilePictureUri(200, 200).toString());
+            Log.v("Name", profile.getName());
+        }
+    }
+
+    private void signUpApi() {
+//        AuthService service = ServiceGenerator.createService(AuthService.class);
+//        // create a map of data to pass along
+//        RequestBody username = createPartFromString(et.getText().toString());
+//        RequestBody password = createPartFromString(et2.getText().toString());
+//        RequestBody email = createPartFromString(et4.getText().toString());
+//        RequestBody role = createPartFromString("U");
+//        RequestBody sex = createPartFromString(this.sex);
+//        HashMap<String, RequestBody> map = new HashMap<>();
+//        map.put("username", username);
+//        map.put("password", password);
+//        map.put("email", email);
+//        map.put("role", role);
+//        map.put("sex", sex);
+//
+//        // finally, execute the request
+//        Call<ResponseBody> call = service.Signup(map, body);
+    }
+
+    @NonNull
+    private RequestBody createPartFromString(String descriptionString) {
+        return RequestBody.create(MediaType.parse(MULTIPART_FORM_DATA), descriptionString);
+    }
+
 
 }
